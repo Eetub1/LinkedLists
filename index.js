45768127c2ad53cffcf82c1f5aaa53119dc290cc
Eetu <eetu.oinonen04@gmail.com>
2025-07-26 13:16:00 +0300

    project is ready

--- index.js
+++ index.js
@@ -41,26 +41,99 @@ class LinkedList {
     tail() {return this.lastNode}
 
     at(index) {
-            if (this.firstNode !== null) {
-            let count = 0
-            let current = this.firstNode
-            while (true) {
-                if (count === index) return current
-                count++
-                if (current.nextNode === null) break
-                else {current = current.nextNode}
-            }
-       } 
-       return null
+        let size = this.size()
+        if (index >= size) return null
+        let current = this.head()
+        for (let i = 0; i < size; i++) {
+            if (i === index) return current
+            current = current.nextNode 
+        }
+        return null
     }
 
-    pop() {}
+    pop() {
+        const size = this.size()
+        if (size < 2) {
+            this.firstNode = null
+            this.lastNode = null
+        } else {
+            const secondLast = this.at(size - 2)
+            secondLast.nextNode = null
+            this.lastNode = secondLast
+        }
+    }
 
-    contains(value) {}
+    contains(value) {
+        const size = this.size()
+        if (size < 1) return false
+        let current = this.head()
+        for (let i = 0; i < size; i++) {
+            if (current.value === value) return true
+            current = current.nextNode
+        }
+        return false
+    }
 
-    find(value) {}
+    find(value) {
+        const size = this.size()
+        if (size < 1) return null
+        let current = this.head()
+        for (let i = 0; i < size; i++) {
+            if (current.value === value) return i
+            current = current.nextNode
+        }
+        return null
+    }
 
-    toString() {}
+    toString() {
+        const size = this.size()
+        if (size < 1) return "( null )"
+        let listAsString = ""
+        let current = this.head()
+        for (let i = 0; i < size; i++) {
+            listAsString += `( ${current.value} ) -> `
+            current = current.nextNode
+        }
+        listAsString += "null"
+        return listAsString
+    }
+
+    insertAt(value, index) {
+        const newNode = new Node(value)
+        const size = this.size()
+        if (size === 0 && index === 0) {
+            this.append(newNode)
+        } else if (index >= size) {
+            return
+        } else if (index === 0) {
+            const currentFirst = this.head()
+            this.firstNode = newNode
+            newNode.nextNode = currentFirst
+        } else {
+            const prev = this.at(index - 1)
+            const next = this.at(index)
+            prev.nextNode = newNode
+            newNode.nextNode = next
+        }
+    }
+
+    removeAt(index) {
+        const size = this.size()
+        if (index >= size || index < 0) return
+        if (size === 1) {
+            this.firstNode = null
+            this.lastNode = null
+        } else if (index === size - 1) {
+            const prev = this.at(index - 1)
+            prev.nextNode = null
+            this.lastNode = prev
+        } else {
+            const prev = this.at(index - 1)
+            const next = this.at(index + 1)
+            prev.nextNode = next
+
+        }
+    }
 }
 
 class Node {
@@ -77,10 +150,38 @@ function main() {
     list.append(2)
     list.append(3)
     list.prepend(0)
+
     console.log(list.size())
     console.log(list.head().value)
     console.log(list.tail().value)
-    console.log(list.at(2))
+
+    console.log("Ennen poistoa: ", list.at(4))
+    list.pop()
+    console.log("Poiston jälkeen: ", list.at(4))
+
+    console.log(list.contains(10))
+    console.log(list.contains(5))
+
+    console.log(list.find(0))
+    console.log(list.find(2))
+    console.log(list.find(12))
+
+    console.log(list.toString())
+
+    list.insertAt(99, 0)
+    console.log(list.toString())
+
+    list.insertAt(90, 2)
+    console.log(list.toString())
+
+    list.removeAt(5)
+    console.log(list.toString())
+
+    list.removeAt(2)
+    console.log(list.toString())
+
+    list.removeAt(3)
+    console.log(list.toString())
 }
 
 main()
